08d77a0dd2895418d646188dec1b48c2893dc27a
Lucas Brendel <lucasbrendel@gmail.com>
2018-12-21 18:06:12 -0600

    Adding types for rtc

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,6 @@ pub use xmc4100;
 #[cfg(feature = "xmc4100")]
 pub use xmc4100 as device;
 
+pub mod rtc;
 pub mod scu;
 pub mod time;
---
+++ src/rtc.rs
@@ -0,0 +1,35 @@
+
+
+pub enum Event{
+    Seconds,
+    Minutes,
+    Hours,
+    Days,
+    Months,
+    Years
+}
+
+pub enum Month {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
+
+pub enum Weekday{
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
