5df20287c72883c07cfbd872f930b32f9b3d0839
Lucas Brendel <lucasbrendel@gmail.com>
2018-12-22 22:27:00 -0600

    adding more apis

--- src/rtc.rs
+++ src/rtc.rs
@@ -1,5 +1,12 @@
 use crate::device::{RTC, SCU_GENERAL};
 
+const MAX_SECONDS: u8 = 59;
+const MAX_MINUTES: u8 = 59;
+const MAX_HOURS: u8 = 23;
+const MAX_DAYS: u8 = 31;
+const MAX_YEAR: u16 = 65535;
+const YEAR_OFFSET: u16 = 1900;
+
 pub enum Event {
     Seconds,
     Minutes,
@@ -32,6 +39,16 @@ pub enum Weekday {
     Thursday,
     Friday,
     Saturday,
+}
+
+pub struct Time {
+    second: u8,
+    minute: u8,
+    hour: u8,
+    day: u8,
+    weekday: Weekday,
+    month: Month,
+    year: u16,
 }
 
 pub trait RtcExt {
@@ -75,5 +92,43 @@ impl Rtc {
             // Check SCU_MIRRSTS to ensure that no transfer over serial interface is pending
         }
         rtc.ctr.read().enb().bit_is_set()
+    }
+
+    pub fn set_prescaler(prescaler: u16) {
+        let scu_gen = unsafe { &*SCU_GENERAL::ptr() };
+        while scu_gen.mirrsts.read().rtc_ctr().bit_is_clear() {
+            // Check SCU_MIRRSTS to ensure that no transfer over serial interface is pending
+        }
+        let rtc = unsafe { &*RTC::ptr() };
+        rtc.ctr.modify(|_, w| unsafe { w.div().bits(prescaler) });
+    }
+
+    pub fn set_time(time: Time) {
+        assert!(time.second < MAX_SECONDS);
+        assert!(time.minute < MAX_MINUTES);
+        assert!(time.hour < MAX_HOURS);
+        assert!(time.day < MAX_DAYS);
+        assert!(time.year < MAX_YEAR);
+
+        let scu_gen = unsafe { &*SCU_GENERAL::ptr() };
+        while scu_gen.mirrsts.read().rtc_ctr().bit_is_clear() {
+            // Check SCU_MIRRSTS to ensure that no transfer over serial interface is pending
+        }
+        let rtc = unsafe { &*RTC::ptr() };
+        // TODO: Not sure if this is fully correct. The C code does a single struct assignment.
+        rtc.tim0.modify(|_, w| unsafe {
+            w.se().bits(time.second);
+            w.mi().bits(time.minute);
+            w.ho().bits(time.hour);
+            w.da().bits(time.day)
+        });
+        while scu_gen.mirrsts.read().rtc_ctr().bit_is_clear() {
+            // Check SCU_MIRRSTS to ensure that no transfer over serial interface is pending
+        }
+        rtc.tim1.modify(|_, w| unsafe {
+            w.dawe().bits(time.weekday as u8);
+            w.mo().bits(time.month as u8);
+            w.ye().bits(time.year)
+        });
     }
 }
