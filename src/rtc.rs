56af3e99b5073cc505ea8a51d936ed11f487e636
Lucas Brendel <lucasbrendel@gmail.com>
2018-12-22 21:53:41 -0600

    Implementing initial Rtc Apis

--- src/rtc.rs
+++ src/rtc.rs
@@ -1,12 +1,12 @@
+use crate::device::{RTC, SCU_GENERAL};
 
-
-pub enum Event{
+pub enum Event {
     Seconds,
     Minutes,
     Hours,
     Days,
     Months,
-    Years
+    Years,
 }
 
 pub enum Month {
@@ -21,15 +21,59 @@ pub enum Month {
     September,
     October,
     November,
-    December
+    December,
 }
 
-pub enum Weekday{
+pub enum Weekday {
     Sunday,
     Monday,
     Tuesday,
     Wednesday,
     Thursday,
     Friday,
-    Saturday
+    Saturday,
+}
+
+pub trait RtcExt {
+    fn constrain(self) -> Rtc;
+}
+
+impl RtcExt for Rtc {
+    fn constrain(self) -> Rtc {
+        Rtc {}
+    }
+}
+
+pub struct Rtc {}
+
+impl Rtc {
+    pub fn start(self) {
+        let rtc = unsafe { &*RTC::ptr() };
+        let scu_gen = unsafe { &*SCU_GENERAL::ptr() };
+
+        while scu_gen.mirrsts.read().rtc_ctr().bit_is_clear() {
+            // Check SCU_MIRRSTS to ensure that no transfer over serial interface is pending
+        }
+        rtc.ctr.modify(|_, w| w.enb().set_bit());
+    }
+
+    pub fn stop(self) {
+        let rtc = unsafe { &*RTC::ptr() };
+        let scu_gen = unsafe { &*SCU_GENERAL::ptr() };
+
+        while scu_gen.mirrsts.read().rtc_ctr().bit_is_clear() {
+            // Check SCU_MIRRSTS to ensure that no transfer over serial interface is pending
+        }
+        rtc.ctr.modify(|_, w| w.enb().clear_bit());
+    }
+
+    pub fn is_running(self) -> bool {
+        let rtc = unsafe { &*RTC::ptr() };
+        let scu_gen = unsafe { &*SCU_GENERAL::ptr() };
+
+        while scu_gen.mirrsts.read().rtc_ctr().bit_is_clear() {
+            // Check SCU_MIRRSTS to ensure that no transfer over serial interface is pending
+        }
+        rtc.ctr.read().enb().bit_is_set()
+    }
 }
