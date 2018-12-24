86f66bf6b840adad50768f7f9b0cbe638118eeca
Lucas Brendel <lucasbrendel@gmail.com>
2018-12-24 13:36:35 -0600

    Removed Scu dependencies for direct access

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,20 @@ pub use xmc4100;
 #[cfg(feature = "xmc4100")]
 pub use xmc4100 as device;
 
+#[cfg(feature = "xmc4200")]
+pub use xmc4200;
+#[cfg(feature = "xmc4200")]
+pub use xmc4200 as device;
+
+#[cfg(feature = "xmc4500")]
+pub use xmc4500;
+#[cfg(feature = "xmc4500")]
+pub use xmc4500 as device;
+
+#[cfg(feature = "xmc4700")]
+pub use xmc4700;
+#[cfg(feature = "xmc4700")]
+pub use xmc4700 as device;
+
 pub mod rtc;
 pub mod scu;
