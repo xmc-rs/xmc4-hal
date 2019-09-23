aa8192bcaa707574538353f6fc6c0f1caf919af5
Lucas Brendel <lucasbrendel@gmail.com>
2019-09-23 13:57:37 -0500

    New Devices (#36)
    * adding missing devices
    
    * adding device selection during builds without features
    
    * adding missing information
    
    * adding github action to build all devices on master
    
    * adding single device build for PR

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 #![no_std]
+#![allow(non_camel_case_types)]
+
 #[cfg(feature = "xmc4100")]
 pub use xmc4100;
 #[cfg(feature = "xmc4100")]
@@ -8,6 +10,16 @@ pub use xmc4100 as device;
 pub use xmc4200;
 #[cfg(feature = "xmc4200")]
 pub use xmc4200 as device;
+
+#[cfg(feature = "xmc4300")]
+pub use xmc4300;
+#[cfg(feature = "xmc4300")]
+pub use xmc4300 as device;
+
+#[cfg(feature = "xmc4400")]
+pub use xmc4400;
+#[cfg(feature = "xmc4400")]
+pub use xmc4400 as device;
 
 #[cfg(feature = "xmc4500")]
 pub use xmc4500;
@@ -19,5 +31,12 @@ pub use xmc4700;
 #[cfg(feature = "xmc4700")]
 pub use xmc4700 as device;
 
+#[cfg(feature = "xmc4800")]
+pub use xmc4800;
+#[cfg(feature = "xmc4800")]
+pub use xmc4800 as device;
+
+#[cfg(feature = "device-selected")]
 pub mod rtc;
+#[cfg(feature = "device-selected")]
 pub mod scu;
