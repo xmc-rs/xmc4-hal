6ccc46eac838f7d0a886d1ea818573702550287a
lb08208 <BrendelLucasA@JohnDeere.com>
2019-10-05 13:37:17 -0500

    Adding a bunch of enumerations

--- src/vadc.rs
+++ src/vadc.rs
@@ -14,6 +14,98 @@ pub struct Vadc {}
 
 // IMPLEMENT PERIPHERAL AFTER THIS LINE
 
+/// Types of service requests. Each group can raise up to 4 service requests and all groups together can have 4 service
+/// requests.
+pub enum ServiceRequest {
+    GroupSR0,
+    GroupSR1,
+    GroupSR2,
+    GroupSR3,
+    SharedSR0,
+    SharedSR1,
+    SharedSR2,
+    SharedSR3
+}
+
+/// Operational mode of a channel when a conversion is interrupted.
+pub enum StartMode {
+    /// Conversion completes without interruption
+    WithoutInterruption,
+    /// Conversion can be interrupted and then resumed.
+    InterruptionResume,
+    /// Conversion can be interrupted and not resume.
+    InterruptionStop,
+}
+
+/// Types of edges that can start conversion based on an external source.
+pub enum TriggerEdge {
+    /// No external triggers
+    None,
+    /// Conversion starts on a falling edge of external source
+    Falling,
+    /// Conversion starts on rising edge of external source
+    Rising,
+    /// Falling and rising edge of external source can start conversion
+    Any
+}
+
+/// Different possibilities that can act as an external input for triggering conversion. The results of the chosen item
+/// differ per channel.
+pub enum  TriggerInputSelect {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    I,
+    J,
+    K,
+    L,
+    M,
+    N,
+    O,
+    P
+}
+
+/// Different gating input possibilities that can gate conversion requests.
+pub enum GateInputSelect {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    I,
+    J,
+    K,
+    L,
+    M,
+    N,
+    O,
+    P
+}
+
+/// Conditions for gating conversion requests.
+pub enum GateMode {
+    /// No external triggers are passed
+    Block,
+    /// All external triggers are passed
+    Ignore,
+    /// Gate signals that are active high are passed
+    ActiveHigh,
+    /// Gate signals that are active low are passed
+    ActiveLow
+}
+
+impl Vadc {
+
+}
+
 
 #[cfg(test)]
 mod tests {
