329a4ab58e3b3a25ddcbc2b7737b97aea5ac1952
lucasbrendel <lucasbrendel@gmail.com>
2019-10-25 07:54:03 -0500

    adding initial enums and methods

--- src/wdt.rs
+++ src/wdt.rs
@@ -1,4 +1,4 @@
-
+use crate::device::{WDT};
 
 pub trait WdtExt {
     fn constrain(self) -> Wdt;
@@ -14,6 +14,46 @@ pub struct Wdt {}
 
 // IMPLEMENT PERIPHERAL AFTER THIS LINE
 
+// Todo: Verify the values of the enum
+pub enum Mode {
+    Timeout,
+    Prewarning
+}
+
+// Todo: Verify the values of the enum
+pub enum DebugMode {
+    Stop,
+    Run
+}
+
+pub enum EventMode {
+    Interrupt,
+    NmiRequest
+}
+
+pub enum Status {
+    Success,
+    Failure
+}
+
+impl Wdt {
+    pub fn enable() {
+
+    }
+
+    pub fn disable() {
+
+    }
+
+    pub fn set_window_bounds(lower: u32, upper: u32) {
+        let wdt = unsafe {&*WDT::ptr()};
+        unsafe { wdt.wlb.modify(|_, w| w.bits(lower))};
+    }
+
+    pub fn start() {
+
+    }
+}
 
 #[cfg(test)]
 mod tests {
