d637132eb72dc0bd048d0a1cdc3f367ffdf05b29
Lucas Brendel <lucasbrendel@gmail.com>
2019-10-28 07:46:42 -0500

    WDT (#46)
    * Adding a bunch of enumerations
    
    * Adding clock func
    
    * RLS config
    
    * addings docs
    
    * Adding sleep functions
    
    * adding clock init
    
    * adding some from impls
    
    * adding more from traits
    
    * fixing errors
    
    * removed unreachable matches
    
    * formatting file
    
    * changed register sets/clears to macro
    
    * impl clock init
    
    * disabling startup calibration
    
    * removing unnecessary dependencies
    
    * addign attributes to enums
    
    * adding initial enums and methods
    
    * activating default feature for rls
    
    * allowing dead code
    
    * adding new macro for setting whole register
    
    * adding feature build for clippy action
    
    * formatting code
    
    * formatting code
    
    * adding visual studio tasks
    
    * implementing more apis for CTR
    
    * finished 2 more apis for CTR
    
    * adding more api's and improving macros
    
    * formatting code
    
    * adding constants
    
    * updating api and adding example
    
    * Removing comment
    
    * allowing clippy lint
    
    * Making clippy and fmt on PR only
    
    * coverage only builds tests not examples
    
    * removing todo statement
    
    * removing duplicate mod inclusion

--- src/macros.rs
+++ src/macros.rs
@@ -1,15 +1,48 @@
+/// Get pointer to peripheral register set
+macro_rules! periph {
+    ($periph:ident) => {
+        unsafe { &*$periph::ptr() }
+    };
+}
+
 /// Set the bit of a register
 macro_rules! set {
     ($periph:ident, $reg:ident, $bits:ident) => {
-        let periph = unsafe { &*$periph::ptr() };
-        periph.$reg.modify(|_, w| w.$bits().set_bit());
+        let periph = periph!($periph);
+        periph.$reg.write(|w| w.$bits().set_bit());
     };
 }
 
 /// Reset the bit of a register
 macro_rules! clear {
     ($periph:ident, $reg:ident, $bits:ident) => {
-        let periph = unsafe { &*$periph::ptr() };
-        periph.$reg.modify(|_, w| w.$bits().clear_bit());
+        let periph = periph!($periph);
+        periph.$reg.write(|w| w.$bits().clear_bit());
     };
 }
+
+macro_rules! set_reg {
+    ($periph:ident, $reg:ident, $value:expr) => {
+        let periph = periph!($periph);
+        unsafe { periph.$reg.write(|w| w.bits($value)) };
+    };
+}
+
+macro_rules! set_field {
+    ($periph:ident, $reg:ident, $bits:ident, $value:expr) => {
+        let periph = periph!($periph);
+        unsafe { periph.$reg.write(|w| w.$bits().bits($value)) };
+    };
+}
+
+macro_rules! get_reg {
+    ($periph:ident, $reg:ident) => {
+        unsafe { &*$periph::ptr() }.$reg.read().bits()
+    };
+}
+
+// macro_rules! get_field {
+//     ($periph:ident, $reg:ident, $field:ident) => {
+//         unsafe { &*$periph::ptr() }.$reg.read().$field.bits()
+//     };
+// }
--- src/wdt.rs
+++ src/wdt.rs
@@ -1,24 +1,133 @@
-pub trait WdtExt {
-    fn constrain(self) -> Wdt;
+#![allow(dead_code)]
+
+use crate::device::wdt::RegisterBlock;
+use crate::device::WDT;
+
+const ALARM_CLEAR: u32 = 2;
+
+const SERVICE_KEY: u32 = 0xABAD_CAFE;
+
+pub struct Wdt {
+    wdt: *const RegisterBlock,
 }
 
-impl WdtExt for Wdt {
-    fn constrain(self) -> Wdt {
-        Wdt {}
+// Todo: Verify the values of the enum
+#[derive(PartialEq)]
+pub enum Mode {
+    Timeout,
+    Prewarning,
+}
+
+impl From<Mode> for u32 {
+    fn from(bits: Mode) -> Self {
+        match bits {
+            Mode::Timeout => 0,
+            Mode::Prewarning => 1,
+        }
     }
 }
 
-pub struct Wdt {}
+impl From<u32> for Mode {
+    fn from(bits: u32) -> Self {
+        match bits {
+            0 => Mode::Timeout,
+            1 => Mode::Prewarning,
+            _ => unimplemented!(),
+        }
+    }
+}
 
-// IMPLEMENT PERIPHERAL AFTER THIS LINE
+#[derive(PartialEq)]
+pub enum DebugMode {
+    Stop,
+    Run,
+}
+
+pub enum EventMode {
+    Interrupt,
+    NmiRequest,
+}
+
+pub enum Status {
+    Success,
+    Failure,
+}
+
+impl Wdt {
+    pub fn new() -> Wdt {
+        // Haven't resolved yet fully how i want to deal with this.
+        // Need to do more reading.
+        let w = Wdt { wdt: WDT::ptr() };
+        w.enable();
+        w
+    }
+
+    // TODO: Implement Scu API's
+    pub fn enable(&self) {
+        //Scu Clock enable_clock()
+        //Scu Clock ungate_peripheral_clock
+        //Scu Clock deassert_peripheral_reset
+    }
+
+    // TODO Implement Scu API's
+    pub fn disable(&self) {
+        //Scu Clock assert_peripheral_reset
+        //Scu Clock gate_peripheral_clock
+        //Scu Clock disable_clock()
+    }
+
+    pub fn set_window_bounds(self, lower: u32, upper: u32) {
+        set_reg!(WDT, wlb, lower);
+        set_reg!(WDT, wub, upper);
+    }
+
+    pub fn start(self) {
+        set!(WDT, ctr, enb);
+    }
+
+    pub fn stop(self) {
+        clear!(WDT, ctr, enb);
+    }
+
+    pub fn set_mode(self, mode: Mode) {
+        if Mode::Timeout == mode {
+            clear!(WDT, ctr, pre);
+        } else {
+            set!(WDT, ctr, pre);
+        }
+    }
+
+    pub fn set_service_pulse_width(self, pulse_width: u8) {
+        set_field!(WDT, ctr, spw, pulse_width);
+    }
+
+    pub fn set_debug_mode(self, mode: DebugMode) {
+        if DebugMode::Run == mode {
+            set!(WDT, ctr, dsp);
+        } else {
+            clear!(WDT, ctr, dsp);
+        }
+    }
+
+    pub fn get_counter(self) -> u32 {
+        get_reg!(WDT, tim)
+    }
+
+    pub fn service(self) {
+        set_reg!(WDT, srv, SERVICE_KEY);
+    }
+
+    pub fn clear_alarm(self) {
+        set_reg!(WDT, wdtclr, ALARM_CLEAR);
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
     #[test]
     fn nothing() {
-        // Do nothing test
+        // Exists just to track coverage
     }
 }
