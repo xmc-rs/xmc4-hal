25ca38c2d455ae59738e35475214fe396f1c126d
lucasbrendel <lucasbrendel@gmail.com>
2019-10-29 07:48:23 -0500

    adding lots of enums

--- src/scu.rs
+++ src/scu.rs
@@ -1,5 +1,12 @@
 use crate::device::{SCU_POWER, SCU_RESET};
 
+const PLL_PDIV_XTAL_8MHZ: u32 = 1;
+/// PDIV for main PLL
+const PLL_NDIV_XTAL_8MHZ: u32 = 89;
+/// NDIV for main PLL
+const PLL_K2DIV_XTAL_8MHZ: u32 = 2;
+/// K2DIV for main PLL
+
 pub trait ScuExt {
     fn constrain(self) -> Scu;
 }
@@ -10,6 +17,289 @@ impl ScuExt for Scu {
     fn constrain(self) -> Scu {
         Scu {}
     }
+}
+
+pub enum Status {
+    /// Scu operations completed
+    Ok,
+    /// Scu operations failed or cannot be fulfilled.
+    Error,
+    /// Cannot complete request because another one is in progress
+    Busy,
+}
+
+/// Triggers for global start of control of specified Ccu channels
+pub enum CcuTrigger {
+    Ccu40,
+    Ccu41,
+    Ccu42,
+    Ccu43,
+    Ccu80,
+    Ccu81,
+}
+
+pub enum Trap {
+    OscWdg,
+    VcoLock,
+    UsbVcoLock,
+    ParityError,
+    Brownout,
+    UlpWdg,
+    PerBridge0,
+    PerBridge1,
+    DieTempHigh,
+    DieTempLow,
+    EcatReset,
+}
+
+pub enum Parity {
+    PsramMem,
+    Dsram1Mem,
+    Dsram2Mem,
+    Usic0Mem,
+    Usic1Mem,
+    Usic2Mem,
+    McanMem,
+    PmuMem,
+    UsbMem,
+    EthTxMem,
+    EthRxMem,
+    SdmmcMem0,
+    SdmmcMem1,
+}
+
+pub enum ResetReason {
+    Porst,
+    Swd,
+    Pv,
+    Sw,
+    Lockup,
+    Watchdog,
+    ParityError,
+}
+
+pub enum NmiReq {
+    WdtWarn,
+    RtcPi,
+    RtcAi,
+    Eru00,
+    Eru01,
+    Eru02,
+    Eru03,
+}
+
+pub enum PeripheralReset {
+    Vadc,
+    Dsd,
+    Ccu40,
+    Ccu41,
+    Ccu42,
+    Ccu80,
+    Ccu81,
+    Posif0,
+    Posif1,
+    Usic0,
+    Eru1,
+    Hrpwm0,
+    Ccu43,
+    Ledts0,
+    Mcan,
+    Dac,
+    Sdmmc,
+    Usic1,
+    Usic2,
+    Ports,
+    Wdt,
+    Eth0,
+    Gpdma0,
+    Gpdma1,
+    Fce,
+    Usb0,
+    Ecat0,
+    Ebu,
+}
+
+pub enum Clock {
+    Usb,
+    Mmc,
+    Eth,
+    Ebu,
+    Ccu,
+    Wdt,
+}
+
+pub enum PeripheralClock {
+    Vadc,
+    Dsd,
+    Ccu40,
+    Ccu41,
+    Ccu42,
+    Ccu80,
+    Ccu81,
+    Posif0,
+    Posif1,
+    Usic0,
+    Eru1,
+    Hrpwm0,
+    Ccu43,
+    Ledts0,
+    Mcan,
+    Dac,
+    Sdmmc,
+    Usic1,
+    Usic2,
+    Ports,
+    Wdt,
+    Eth0,
+    Gpdma0,
+    Gpdma1,
+    Fce,
+    Usb0,
+    Ecat0,
+    Ebu,
+}
+
+pub enum SysClockSource {
+    Ofi,
+    Pll,
+}
+
+pub enum SysPllClockSource {
+    Oschp,
+    Ofi,
+}
+
+pub enum UsbClockSource {
+    UsbPll,
+    SysPll,
+}
+
+#[cfg(feature = "ecat")]
+pub enum EcatClockSource {
+    UsbPll,
+    SysPll,
+}
+
+pub enum WdtClockSource {
+    Ofi,
+    Stdby,
+    Pll,
+}
+
+pub enum ExtOutClockSource {
+    Sys,
+    Usb,
+    Pll,
+    #[cfg(feature = "ecat")]
+    Stdby,
+}
+
+pub enum RtcClockSource {
+    Osi,
+    Ulp
+}
+
+pub enum StdbyClockSource {
+    Osi,
+    Osculp,
+}
+
+pub enum FofiCalibrationMode {
+    Factory,
+    Automatic,
+}
+
+pub enum BootMode {
+    Normal,
+    AscBsl,
+    Bmi,
+    CanBsl,
+    PsramBoot,
+    Abm0,
+    Abm1,
+    Fabm
+}
+
+pub enum SysPllMode {
+    Disabled,
+    Normal,
+    Prescaler
+}
+
+pub enum SleepModeConfig {
+    SysClkFofi,
+    SysClkPll,
+    DisableUsb,
+    EnableUsb,
+    DisableSdmmc,
+    EnableSdmmc,
+    DisableEth,
+    EnableEth,
+    DisableEbu,
+    EnableEbu,
+    DisableCcu,
+    EnableCcu,
+    DisableWdt,
+    EnableWdt
+}
+
+pub enum DeepSleepModeConfig {
+    SysClkFofi,
+    SysClkPll,
+    FlashPowerDown,
+    PllPowerDown,
+    VcoPowerDown,
+    DisableUsb,
+    EnableUsb,
+    DisableSdmmc,
+    EnableSdmmc,
+    DisableEth,
+    EnableEth,
+    DisableEbu,
+    EnableEbu,
+    DisableCcu,
+    EnableCcu,
+    DisableWdt,
+    EnableWdt
+}
+
+pub enum PowerEvrStatus {
+    Ok,
+    Evr13OverVoltage,
+}
+
+pub enum CtrlStatus {
+    NoActive,
+    Active
+}
+
+pub enum HibernateEvent{
+    OnPosEdge,
+    OnNegEdge,
+    OnRtc,
+    UlpWdg,
+}
+
+pub enum HibernateIo {
+    Io0,
+    Io1,
+}
+
+pub enum HibernatePinMode {
+    InputPullNone,
+    InputPullDown,
+    InputPullUp,
+    OutputPushPullHibCtrl,
+    OutputPushPullWdtSrv,
+    OutputPushPullGpio,
+    OutputOpenDrainHibCtrl,
+    OutputOpenDrainWdtSrv,
+    OutputOpenDrainGpio,
+}
+
+pub enum HibernateIoOutputLevel {
+    Low,
+    High
 }
 
 impl Scu {
