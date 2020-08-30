5233f32263a5842c8d1eef14573ac3bbb2701668
Lucas Brendel <lucasbrendel@gmail.com>
2020-08-30 06:48:24 -0500

    adding enums

--- src/gpio.rs
+++ src/gpio.rs
@@ -1,16 +1,70 @@
-pub trait GpioExt {
-    fn constrain(self) -> Gpio;
+
+#![allow(dead_code)]
+
+use crate::device::port0::RegisterBlock;
+use crate::device::PORT0;
+
+const IOCR_PC_SIZE: u32 = 8;
+
+pub struct Gpio {
+    port0: *const RegisterBlock
 }
 
-impl GpioExt for Gpio {
-    fn constrain(self) -> Gpio {
-        Gpio {}
+pub enum OutputLevel {
+    /// Reset bit
+    Low = 0x10000,
+    ///Set bit
+    High = 1
+}
+
+impl From<u32> for OutputLevel {
+    fn from(bits: u32) -> Self {
+        match bits {
+            0x10000 => OutputLevel::Low,
+            1 => OutputLevel::High,
+            _ => unimplemented!(),
+        }
     }
 }
 
-pub struct Gpio {}
+impl From<OutputLevel> for u32 {
+    fn from(level: OutputLevel) -> Self {
+        match level {
+            OutputLevel::Low => 0x10000,
+            OutputLevel::High => 1,
+        }
+    }
+}
 
-// IMPLEMENT PERIPHERAL AFTER THIS LINE
+pub enum HardwareControl {
+    /// Software control only.
+    Disabled,
+    /// HWI0/HWO0 control path can override the software configuration
+    Peripheral1,
+    /// HWI1/HWO1 control path can override the software configuration
+    Peripheral2
+}
+
+impl From<u32> for HardwareControl {
+    fn from(bits: u32) -> Self {
+        match bits {
+            0 => HardwareControl::Disabled,
+            1 => HardwareControl::Peripheral1,
+            2 => HardwareControl::Peripheral2,
+            _ => unimplemented!(),
+        }
+    }
+}
+
+impl From<HardwareControl> for u32 {
+    fn from(ctrl: HardwareControl) -> Self {
+        match ctrl {
+            HardwareControl::Disabled => 0,
+            HardwareConrtol::Peripheral1 => 1,
+            HardwareControl::Peripheral2 => 2,
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
