ffac23c02c67bedddcc3490538d8fbf5b756fa2b
Lucas Brendel <lucasbrendel@gmail.com>
2023-12-20 22:46:47 -0600

    feat(scu): adding more apis (#184)
    * fix(scu): adding events
    
    * fix:  implementing event triggers
    
    * chore(scu):  formatting
    
    * fix(scu): renaming enum
    
    * fix(scu): adding get trigger event
    
    * chore(scu): fix formatting
    
    * fix(scu): adding derives to all enums
    
    * fix(scu): adding clear status
    
    * chore: enable format on save
    
    * fix: adding gpr access apis
    
    * fix(scu): removing register macros
    
    * fix(scu): improving xmc43 support
    
    * fix(scu): fixing other variant support
    
    * fix(scu): oops forgot a set
    
    * feat(scu): adding retention memory apis
    
    * fix(scu): correcting operation
    
    * fix(scu): adding clock init
    
    * fix(scu): adding trap apis
    
    * fix(scu): adding missing self arg
    
    * fix(scu): replacing use of macro
    
    * chore(scu): correcting formatting
    
    * fix: adding missing unimplemented
    
    * fix(scu): correcting import
    
    ---------
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- src/scu.rs
+++ src/scu.rs
@@ -1,4 +1,4 @@
-use crate::pac::{SCU_CLK, SCU_GENERAL, SCU_POWER, SCU_RESET};
+use crate::pac::{SCU_CLK, SCU_GENERAL, SCU_INTERRUPT, SCU_POWER, SCU_RESET, SCU_TRAP};
 /// PDIV for main PLL
 const PLL_PDIV_XTAL_8MHZ: u32 = 1;
 
@@ -10,6 +10,7 @@ const PLL_K2DIV_XTAL_8MHZ: u32 = 2;
 
 pub struct Scu {}
 
+#[derive(Copy, Clone, Debug)]
 pub enum Status {
     /// Scu operations completed
     Ok,
@@ -20,6 +21,7 @@ pub enum Status {
 }
 
 /// Triggers for global start of control of specified Ccu channels
+#[derive(Copy, Clone, Debug)]
 pub enum CcuTrigger {
     Ccu40,
     Ccu41,
@@ -29,6 +31,7 @@ pub enum CcuTrigger {
     Ccu81,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum Trap {
     OscWdg,
     VcoLock,
@@ -43,6 +46,7 @@ pub enum Trap {
     EcatReset,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum Parity {
     PsramMem,
     Dsram1Mem,
@@ -59,6 +63,7 @@ pub enum Parity {
     SdmmcMem1,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum ResetReason {
     Porst,
     Swd,
@@ -69,6 +74,7 @@ pub enum ResetReason {
     ParityError,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum NmiReq {
     WdtWarn,
     RtcPi,
@@ -79,6 +85,7 @@ pub enum NmiReq {
     Eru03,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum PeripheralReset {
     Vadc,
     Dsd,
@@ -110,6 +117,7 @@ pub enum PeripheralReset {
     Ebu,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum Clock {
     Usb,
     Mmc,
@@ -133,6 +141,7 @@ impl From<Clock> for u32 {
 }
 
 #[cfg(not(feature = "xmc4500"))]
+#[derive(Copy, Clone, Debug)]
 pub enum PeripheralClock {
     Vadc,
     Dsd,
@@ -164,33 +173,39 @@ pub enum PeripheralClock {
     Ebu,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum SysClockSource {
     Ofi,
     Pll,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum SysPllClockSource {
     Oschp,
     Ofi,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum UsbClockSource {
     UsbPll,
     SysPll,
 }
 
 #[cfg(feature = "ecat")]
+#[derive(Copy, Clone, Debug)]
 pub enum EcatClockSource {
     UsbPll,
     SysPll,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum WdtClockSource {
     Ofi,
     Stdby,
     Pll,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum ExtOutClockSource {
     Sys,
     Usb,
@@ -199,21 +214,25 @@ pub enum ExtOutClockSource {
     Stdby,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum RtcClockSource {
     Osi,
     Ulp,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum StdbyClockSource {
     Osi,
     Osculp,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum FofiCalibrationMode {
     Factory,
     Automatic,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum BootMode {
     Normal,
     AscBsl,
@@ -256,12 +275,14 @@ impl From<u8> for BootMode {
     }
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum SysPllMode {
     Disabled,
     Normal,
     Prescaler,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum SleepModeConfig {
     SysClkFofi,
     SysClkPll,
@@ -279,6 +300,7 @@ pub enum SleepModeConfig {
     EnableWdt,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum DeepSleepModeConfig {
     SysClkFofi,
     SysClkPll,
@@ -299,16 +321,19 @@ pub enum DeepSleepModeConfig {
     EnableWdt,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum PowerEvrStatus {
     Ok,
     Evr13OverVoltage,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum CtrlStatus {
     NoActive,
     Active,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum HibernateEvent {
     OnPosEdge,
     OnNegEdge,
@@ -316,11 +341,13 @@ pub enum HibernateEvent {
     UlpWdg,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum HibernateIo {
     Io0,
     Io1,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum HibernatePinMode {
     InputPullNone,
     InputPullDown,
@@ -333,14 +360,216 @@ pub enum HibernatePinMode {
     OutputOpenDrainGpio,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum HibernateIoOutputLevel {
     Low,
     High,
 }
 
+#[derive(Copy, Clone, Debug)]
+pub enum InterruptEvent {
+    WdtPreWarn,
+    RtcPeriodic,
+    RtcAlarm,
+    DlrRequestOverrun,
+    #[cfg(not(feature = "xmc4300"))]
+    #[cfg(not(feature = "xmc4500"))]
+    #[cfg(not(feature = "xmc4700"))]
+    Lpaclr = 6,
+    #[cfg(not(feature = "xmc4300"))]
+    #[cfg(not(feature = "xmc4500"))]
+    #[cfg(not(feature = "xmc4700"))]
+    Lpacth0,
+    #[cfg(not(feature = "xmc4300"))]
+    #[cfg(not(feature = "xmc4500"))]
+    #[cfg(not(feature = "xmc4700"))]
+    Lpacth1,
+    #[cfg(not(feature = "xmc4300"))]
+    #[cfg(not(feature = "xmc4500"))]
+    #[cfg(not(feature = "xmc4700"))]
+    Lpacst,
+    #[cfg(not(feature = "xmc4300"))]
+    #[cfg(not(feature = "xmc4500"))]
+    #[cfg(not(feature = "xmc4700"))]
+    Lpacclr,
+    #[cfg(not(feature = "xmc4300"))]
+    #[cfg(not(feature = "xmc4500"))]
+    #[cfg(not(feature = "xmc4700"))]
+    Lpacset,
+    #[cfg(not(feature = "xmc4300"))]
+    #[cfg(not(feature = "xmc4500"))]
+    #[cfg(not(feature = "xmc4700"))]
+    Hintst,
+    #[cfg(not(feature = "xmc4300"))]
+    #[cfg(not(feature = "xmc4500"))]
+    #[cfg(not(feature = "xmc4700"))]
+    Hintclr,
+    #[cfg(not(feature = "xmc4300"))]
+    #[cfg(not(feature = "xmc4500"))]
+    #[cfg(not(feature = "xmc4700"))]
+    Hintset,
+    Hdcrclr = 17,
+    Hdcrset,
+    Hdcr,
+    Oscsictrl = 21,
+    Osculctrl = 23,
+    RtcCtr,
+    RtcAtim0,
+    RtcAtim1,
+    RtcTim0,
+    RtcTim1,
+    RetentionMemory,
+}
+
+impl From<InterruptEvent> for u32 {
+    fn from(bits: InterruptEvent) -> u32 {
+        match bits {
+            InterruptEvent::WdtPreWarn => 0,
+            InterruptEvent::RtcPeriodic => 1,
+            InterruptEvent::RtcAlarm => 2,
+            InterruptEvent::DlrRequestOverrun => 3,
+            #[cfg(not(feature = "xmc4300"))]
+            #[cfg(not(feature = "xmc4500"))]
+            #[cfg(not(feature = "xmc4700"))]
+            InterruptEvent::Lpaclr => 6,
+            #[cfg(not(feature = "xmc4300"))]
+            #[cfg(not(feature = "xmc4500"))]
+            #[cfg(not(feature = "xmc4700"))]
+            InterruptEvent::Lpacth0 => 7,
+            #[cfg(not(feature = "xmc4300"))]
+            #[cfg(not(feature = "xmc4500"))]
+            #[cfg(not(feature = "xmc4700"))]
+            InterruptEvent::Lpacth1 => 8,
+            #[cfg(not(feature = "xmc4300"))]
+            #[cfg(not(feature = "xmc4500"))]
+            #[cfg(not(feature = "xmc4700"))]
+            InterruptEvent::Lpacst => 9,
+            #[cfg(not(feature = "xmc4300"))]
+            #[cfg(not(feature = "xmc4500"))]
+            #[cfg(not(feature = "xmc4700"))]
+            InterruptEvent::Lpacclr => 10,
+            #[cfg(not(feature = "xmc4300"))]
+            #[cfg(not(feature = "xmc4500"))]
+            #[cfg(not(feature = "xmc4700"))]
+            InterruptEvent::Lpacset => 11,
+            #[cfg(not(feature = "xmc4300"))]
+            #[cfg(not(feature = "xmc4500"))]
+            #[cfg(not(feature = "xmc4700"))]
+            InterruptEvent::Hintst => 12,
+            #[cfg(not(feature = "xmc4300"))]
+            #[cfg(not(feature = "xmc4500"))]
+            #[cfg(not(feature = "xmc4700"))]
+            InterruptEvent::Hintclr => 13,
+            #[cfg(not(feature = "xmc4300"))]
+            #[cfg(not(feature = "xmc4500"))]
+            #[cfg(not(feature = "xmc4700"))]
+            InterruptEvent::Hintset => 14,
+            InterruptEvent::Hdcrclr => 17,
+            InterruptEvent::Hdcrset => 18,
+            InterruptEvent::Hdcr => 19,
+            InterruptEvent::Oscsictrl => 21,
+            InterruptEvent::Osculctrl => 23,
+            InterruptEvent::RtcCtr => 24,
+            InterruptEvent::RtcAtim0 => 25,
+            InterruptEvent::RtcAtim1 => 26,
+            InterruptEvent::RtcTim0 => 27,
+            InterruptEvent::RtcTim1 => 28,
+            InterruptEvent::RetentionMemory => 29,
+        }
+    }
+}
+
+impl From<u32> for InterruptEvent {
+    fn from(bits: u32) -> InterruptEvent {
+        match bits {
+            0 => InterruptEvent::WdtPreWarn,
+            1 => InterruptEvent::RtcPeriodic,
+            2 => InterruptEvent::RtcAlarm,
+            3 => InterruptEvent::DlrRequestOverrun,
+            #[cfg(not(feature = "xmc4300"))]
+            #[cfg(not(feature = "xmc4500"))]
+            #[cfg(not(feature = "xmc4700"))]
+            6 => InterruptEvent::Lpaclr,
+            #[cfg(not(feature = "xmc4500"))]
+            #[cfg(not(feature = "xmc4300"))]
+            #[cfg(not(feature = "xmc4700"))]
+            7 => InterruptEvent::Lpacth0,
+            #[cfg(not(feature = "xmc4500"))]
+            #[cfg(not(feature = "xmc4300"))]
+            #[cfg(not(feature = "xmc4700"))]
+            8 => InterruptEvent::Lpacth1,
+            #[cfg(not(feature = "xmc4500"))]
+            #[cfg(not(feature = "xmc4300"))]
+            #[cfg(not(feature = "xmc4700"))]
+            9 => InterruptEvent::Lpacst,
+            #[cfg(not(feature = "xmc4500"))]
+            #[cfg(not(feature = "xmc4300"))]
+            #[cfg(not(feature = "xmc4700"))]
+            10 => InterruptEvent::Lpacclr,
+            #[cfg(not(feature = "xmc4500"))]
+            #[cfg(not(feature = "xmc4300"))]
+            #[cfg(not(feature = "xmc4700"))]
+            11 => InterruptEvent::Lpacset,
+            #[cfg(not(feature = "xmc4500"))]
+            #[cfg(not(feature = "xmc4300"))]
+            #[cfg(not(feature = "xmc4700"))]
+            12 => InterruptEvent::Hintst,
+            #[cfg(not(feature = "xmc4500"))]
+            #[cfg(not(feature = "xmc4300"))]
+            #[cfg(not(feature = "xmc4700"))]
+            13 => InterruptEvent::Hintclr,
+            #[cfg(not(feature = "xmc4500"))]
+            #[cfg(not(feature = "xmc4300"))]
+            #[cfg(not(feature = "xmc4700"))]
+            14 => InterruptEvent::Hintset,
+            17 => InterruptEvent::Hdcrclr,
+            18 => InterruptEvent::Hdcrset,
+            19 => InterruptEvent::Hdcr,
+            21 => InterruptEvent::Oscsictrl,
+            23 => InterruptEvent::Osculctrl,
+            24 => InterruptEvent::RtcCtr,
+            25 => InterruptEvent::RtcAtim0,
+            26 => InterruptEvent::RtcAtim1,
+            27 => InterruptEvent::RtcTim0,
+            28 => InterruptEvent::RtcTim1,
+            29 => InterruptEvent::RetentionMemory,
+            _ => unimplemented!(),
+        }
+    }
+}
+
 impl Scu {
     pub fn new() -> Self {
         Scu {}
+    }
+
+    pub fn enable_event(&self, event: InterruptEvent) {
+        let scu = unsafe { &*SCU_INTERRUPT::ptr() };
+
+        scu.srmsk()
+            .modify(|r, w| unsafe { w.bits(r.bits() | event as u32) });
+    }
+
+    pub fn disable_event(&self, event: InterruptEvent) {
+        let scu = unsafe { &*SCU_INTERRUPT::ptr() };
+
+        scu.srmsk()
+            .modify(|r, w| unsafe { w.bits(r.bits() & !(event as u32)) });
+    }
+
+    pub fn trigger_event(&self, event: InterruptEvent) {
+        let scu = unsafe { &*SCU_INTERRUPT::ptr() };
+        scu.srset().write(|w| unsafe { w.bits(event as u32) });
+    }
+
+    pub fn get_event_status(&self) -> InterruptEvent {
+        let scu = unsafe { &*SCU_INTERRUPT::ptr() };
+        scu.srraw().read().bits().into()
+    }
+
+    pub fn clear_event_status(&self, event: InterruptEvent) {
+        let scu = unsafe { &*SCU_INTERRUPT::ptr() };
+        scu.srclr().write(|w| unsafe { w.bits(event as u32) });
     }
 
     pub fn enable_out_of_range_comparator(&self, group: u32, channel: u32) {
@@ -379,6 +608,24 @@ impl Scu {
         }
     }
 
+    pub fn read_gpr(&self, index: u32) -> u32 {
+        let scu = unsafe { &*SCU_GENERAL::ptr() };
+        match index {
+            0 => scu.gpr0().read().bits(),
+            1 => scu.gpr1().read().bits(),
+            _ => unimplemented!(),
+        }
+    }
+
+    pub fn write_gpr(&self, index: u32, data: u32) {
+        let scu = unsafe { &*SCU_GENERAL::ptr() };
+        match index {
+            0 => scu.gpr0().write(|w| unsafe { w.bits(data) }),
+            1 => scu.gpr1().write(|w| unsafe { w.bits(data) }),
+            _ => unimplemented!(),
+        }
+    }
+
     pub fn enable_hibernate_domain(&self) {
         let scu = unsafe { &*SCU_POWER::ptr() };
         if scu.pwrstat().read().hiben().bit_is_clear() {
@@ -387,45 +634,61 @@ impl Scu {
         }
     }
     pub fn is_hibernate_domain_enabled(&self) -> bool {
-        get_field!(SCU_POWER, pwrstat, hiben).bit_is_set()
-            && !get_field!(SCU_RESET, rststat, hibrs).bit_is_set()
+        let power = unsafe { &*SCU_POWER::ptr() };
+        let reset = unsafe { &*SCU_RESET::ptr() };
+
+        power.pwrstat().read().hiben().bit_is_set() && reset.rststat().read().hibrs().bit_is_set()
     }
 
     /// Access the currently programmed boot mode
     pub fn get_boot_mode(&self) -> BootMode {
-        BootMode::from(get_field!(SCU_GENERAL, stcon, swcon).bits())
+        let general = unsafe { &*SCU_GENERAL::ptr() };
+
+        BootMode::from(general.stcon().read().swcon().bits())
     }
 
     /// Program a new device boot mode. The newly set boot mode can be launched by a software reset after updating.
     pub fn set_boot_mode(&self, mode: BootMode) {
-        set_field!(SCU_GENERAL, stcon, swcon, u8::from(mode));
+        let general = unsafe { &*SCU_GENERAL::ptr() };
+
+        general
+            .stcon()
+            .write(|w| unsafe { w.swcon().bits(u8::from(mode)) });
     }
 
     /// Enable a specific clock in the Scu peripheral
     pub fn enable_clock(&self, clock: Clock) {
-        set_reg!(SCU_CLK, clkset, u32::from(clock));
+        let clk = unsafe { &*SCU_CLK::ptr() };
+        clk.clkset().write(|w| unsafe { w.bits(u32::from(clock)) });
     }
 
     /// Disable a specific clock in the Scu peripheral
     pub fn disable_clock(&self, clock: Clock) {
-        set_reg!(SCU_CLK, clkclr, u32::from(clock));
+        let clk = unsafe { &*SCU_CLK::ptr() };
+        clk.clkclr().write(|w| unsafe { w.bits(u32::from(clock)) });
     }
 
     /// Check if a peripheral clock is enabled.
     pub fn is_clock_enabled(&self, clock: Clock) -> bool {
-        (get_reg!(SCU_CLK, clkstat) & u32::from(clock)) > 0
+        let clk = unsafe { &*SCU_CLK::ptr() };
+        (clk.clkstat().read().bits() & u32::from(clock)) > 0
     }
 
     #[cfg(not(feature = "xmc4500"))]
-    pub fn gate_peripheral_clock(&self, _clock: PeripheralClock) {}
+    pub fn gate_peripheral_clock(&self, _clock: PeripheralClock) {
+        unimplemented!();
+    }
 
     #[cfg(not(feature = "xmc4500"))]
-    pub fn ungate_peripheral_clock(&self, _clock: PeripheralClock) {}
+    pub fn ungate_peripheral_clock(&self, _clock: PeripheralClock) {
+        unimplemented!();
+    }
 
     pub fn assert_peripheral_reset(&self, peripheral: PeripheralReset) {
         match peripheral {
             PeripheralReset::Wdt => {
-                set!(SCU_RESET, prset2, wdtrs);
+                let reset = unsafe { &*SCU_RESET::ptr() };
+                reset.prset2().write(|w| w.wdtrs().set_bit());
             }
             _ => unimplemented!(),
         };
@@ -434,50 +697,104 @@ impl Scu {
     pub fn deassert_peripheral_reset(&self, peripheral: PeripheralReset) {
         match peripheral {
             PeripheralReset::Wdt => {
-                set!(SCU_RESET, prclr2, wdtrs);
+                let scu = unsafe { &*SCU_RESET::ptr() };
+                scu.prclr2().write(|w| w.wdtrs().set_bit());
             }
             _ => unimplemented!(),
         };
     }
 
-    fn calibrate_temperature_sensor(_offset: u32, _gain: u32) {
+    fn calibrate_temperature_sensor(&self, _offset: u32, _gain: u32) {
         unimplemented!();
     }
 
-    fn enable_temperature_sensor() {
+    fn enable_temperature_sensor(&self) {
         unimplemented!();
     }
 
-    fn disable_temperature_sensor() {
+    fn disable_temperature_sensor(&self) {
         unimplemented!();
     }
 
-    fn is_temperature_sensor_enabled() -> bool {
+    fn is_temperature_sensor_enabled(&self) -> bool {
         unimplemented!();
     }
 
-    fn start_temperature_measurement() {
+    fn start_temperature_measurement(&self) {
         unimplemented!();
     }
 
-    fn get_temperature_measurement() {
+    fn get_temperature_measurement(&self) {
         unimplemented!()
     }
 
-    fn is_temperature_sensor_busy() -> bool {
+    fn is_temperature_sensor_busy(&self) -> bool {
         unimplemented!();
     }
 
-    fn high_temperature() -> bool {
+    fn high_temperature(&self) -> bool {
         unimplemented!();
     }
 
-    fn set_raw_temp_limits(_lower: u32, _upper: u32) {
+    fn set_raw_temp_limits(&self, _lower: u32, _upper: u32) {
         unimplemented!();
     }
 
-    fn low_temperature() -> bool {
+    fn low_temperature(&self) -> bool {
         unimplemented!();
+    }
+
+    pub fn write_to_retention_memory(&self, address: u32, data: u32) {
+        // TODO: Clean this up
+        let rmacr = ((address << 16) & 0xF0000) | 1;
+        let general = unsafe { &*SCU_GENERAL::ptr() };
+
+        general.rmdata().write(|w| unsafe { w.data().bits(data) });
+        general.rmacr().write(|w| unsafe { w.bits(rmacr) });
+
+        while general.mirrsts().read().rmx().bit_is_set() {}
+    }
+
+    pub fn read_from_retention_memory(&self, address: u32) -> u32 {
+        let rmacr = (address << 16) & 0xF0000;
+        let general = unsafe { &*SCU_GENERAL::ptr() };
+
+        general.rmacr().write(|w| unsafe { w.bits(rmacr) });
+        while general.mirrsts().read().rmx().bit_is_set() {}
+        general.rmdata().read().data().bits()
+    }
+
+    fn clock_init() {
+        unimplemented!();
+    }
+
+    pub fn trap_enable(&self, trap: u32) {
+        let scu = unsafe { &*SCU_TRAP::ptr() };
+
+        scu.trapdis()
+            .modify(|r, w| unsafe { w.bits(r.bits() & !trap) });
+    }
+
+    pub fn trap_disable(&self, trap: u32) {
+        let scu = unsafe { &*SCU_TRAP::ptr() };
+
+        scu.trapdis()
+            .modify(|r, w| unsafe { w.bits(r.bits() | trap) });
+    }
+
+    pub fn trap_get_status(&self) -> u32 {
+        let scu = unsafe { &*SCU_TRAP::ptr() };
+        scu.trapraw().read().bits()
+    }
+
+    pub fn trap_trigger(&self, trap: u32) {
+        let scu = unsafe { &*SCU_TRAP::ptr() };
+        scu.trapset().write(|w| unsafe { w.bits(trap) });
+    }
+
+    pub fn trap_clear_status(&self, trap: u32) {
+        let scu = unsafe { &*SCU_TRAP::ptr() };
+        scu.trapclr().write(|w| unsafe { w.bits(trap) });
     }
 }
 
