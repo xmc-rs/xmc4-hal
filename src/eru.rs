74a00588d3d33ee56b49521603fcdc90f1bd3083
Lucas Brendel <lucasbrendel@gmail.com>
2024-01-13 16:08:39 -0600

    feat(eru): first pass at implementation

--- src/eru.rs
+++ src/eru.rs
@@ -1,3 +1,5 @@
+use crate::pac::{ERU0, ERU1};
+
 pub trait EruExt {
     fn constrain(self) -> Eru;
 }
@@ -8,9 +10,20 @@ impl EruExt for Eru {
     }
 }
 
+pub enum Instance {
+    Eru0,
+    Eru1,
+}
+
 pub struct Eru {}
 
 // IMPLEMENT PERIPHERAL AFTER THIS LINE
+
+impl Eru {
+    pub fn enable(&self) {}
+
+    pub fn disable(&self) {}
+}
 
 #[cfg(test)]
 mod tests {
